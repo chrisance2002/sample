af6679dbb50756e6aeb6338bf931f712963b41b6
chrisance2002 <chrisance2002@gmail>
2024-02-06 12:32:24 +0530

    first commit

---
+++ circle.java
@@ -0,0 +1,10 @@
+public class circle 
+{
+    public static void main(String[] args)
+    {
+        int r=5;
+        float pi=3.14f;
+        float area= pi*r*r;
+        System.out.println("Area of circle: "+area);
+    }    
+}
